d58b4a214c977d4a74b6d1e6fb3e165d39a527aa
pviniciussilva <pedroviniciusdesousarochasilva@gmail.com>
2023-03-28 10:29:12 -0300

    lista 02

--- L2q11.py
+++ L2q11.py
@@ -5,7 +5,8 @@ o programa que calculará os reajustes.
     ◦ salários até R$ 280,00 (incluindo) : aumento de 20%
     ◦ salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
     ◦ salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
-    ◦ salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    ◦ salários de R$ 1500,00 em diante : 
+    ° Aumento de 5% Após o aumento ser realizado, informe na tela;
     ◦ o salário antes do reajuste;
     ◦ o percentual de aumento aplicado;
     ◦ o valor do aumento;
@@ -13,19 +14,24 @@ o programa que calculará os reajustes.
 
 salario_colaborador = float(input('Digite o valor do seu salário: '))
 
-aumento_de_20 = salario_colaborador + ((20/100) * salario_colaborador)
-aumento_de_15 = salario_colaborador + ((15/100) * salario_colaborador)
-aumento_de_10 = salario_colaborador + ((10/100) * salario_colaborador)
-aumento_de_5 = salario_colaborador + ((5/100) * salario_colaborador)
-
 if  salario_colaborador <= 280:
-    print(f'Seu salário sofreu um aumento de 20% e o valor final é de R${aumento_de_20:,.2f}')
-elif salario_colaborador > 700 and salario_colaborador < 280:
-    print(f'Seu salário sofreu um aumento de 15% e o valor final é de R${aumento_de_15:,.2f}')
-elif salario_colaborador > 700 and salario_colaborador < 1500:
-    print(f'Seu salário sofreu um aumento de 10% e o valor final é de R${aumento_de_10:,.2f}')
-elif salario_colaborador >= 1500:
+    novo_salario = salario_colaborador + ((20/100) * salario_colaborador)
+    valor_aumento = ((20/100) * salario_colaborador)
+    percentual = '20%'
+    if salario_colaborador >= 700 and salario_colaborador <= 280:
+        novo_salario = salario_colaborador + ((15/100) * salario_colaborador)
+        valor_aumento = ((15/100) * salario_colaborador)
+        percentual = '15%'
+    if salario_colaborador >= 700 and salario_colaborador <= 1500:
+        novo_salario = salario_colaborador + ((10/100) * salario_colaborador)
+        valor_aumento = ((10/100) * salario_colaborador)
+        percentual = '10%'
+    if salario_colaborador >= 1500:
+        novo_salario = salario_colaborador + ((5/100) * salario_colaborador)
+        valor_aumento = ((5/100) * salario_colaborador)
+        percentual = '5%'
+    
     print(f'O salário antes do reajuste: R${salario_colaborador:,.2f}')
-    print(f'O percentual de aumento aplicado foi de: {(5/100):,.2f}%')
-    print(f'O valor do aumento: R${((5/100) * salario_colaborador):,.2f}')
-    print(f'O novo salário, após o aumento: R${aumento_de_5:,.2f}')
+    print(f'O percentual de aumento aplicado foi de: {percentual}')
+    print(f'O valor do aumento: R${valor_aumento:,.2f}')
+    print(f'O novo salário, após o aumento: R${novo_salario:,.2f}')
