2b217fafeb117681b47820036a0b9878f93c0e44
pviniciussilva <pedroviniciusdesousarochasilva@gmail.com>
2023-04-10 11:43:36 -0300

    atualização de questões

--- L2q16.py
+++ L2q16.py
@@ -1,4 +1,3 @@
-from  import math 
 '''Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. O programa deverá pedir os
 valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:
     ◦ Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau e o programa não deve fazer pedir os
@@ -11,19 +10,17 @@ valores de a, b e c e fazer as consistências, informando ao usuário nas seguin
     
 print('ax2 + bx + c.')
     
-a = float(input('Informe o valor de "a": '))
-b = float(input('Informe o valor de "b": '))
-c = float(input('Informe o valor de "c": '))
-dela = b ** 2 - 4 * a * c
-x = (-(-b) + dela ** (1/2)) / 2 *a
+a = (input('Informe o valor de "a": '))
+b = (input('Informe o valor de "b": '))
+c = (input('Informe o valor de "c": '))
+delta = pow(b,2)- 4 * a * c
+x1 = (-(-b) + delta ** (1/2)) / 2 * a
+x2 = (-(-b) - delta ** (1/2)) / 2 * a
 
-if  a > 0:
-    equaçao = True
-#   if equaçao == True:
-        
-else:
-    equaçao = False
-
-    
-    
-print(f'{equaçao}')
+if x1 > 0:
+    x = x1
+elif x2 > 0:
+    x = x2
+elif a < 0:
+    print('O valor informado à "a" não é válido, a equação não é uma equação de segundo grau') 
+print(f'{x}')
